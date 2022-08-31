49b317c27761bda2daa89185671ef492cc5dc8dc
llopFilms <smdalbars@gmail.com>
2022-08-31 20:15:29 +0200

    Exercici 1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const rebreAcudit = async () => {
+  try {
+    const resposta = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const json = await resposta.json();
+    console.log("Acudit: ", json.joke);
+    document.querySelector("#text-acudit").innerHTML = json.joke;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
