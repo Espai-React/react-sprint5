1241d31b951852afe8a0c0fae13153ac046dfba0
llopFilms <smdalbars@gmail.com>
2022-09-09 23:44:04 +0200

    Exercici 4 + millores exercici 3

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 let acudit;
 const reportAcudits = [];
 
-const mostrarBotons = () => document.querySelectorAll("[puntuacio]").forEach(boto => boto.style.display = "block");
-
-const amagarBotons = () => document.querySelectorAll("[puntuacio]").forEach(boto => boto.style.display = "none");
+const mostrarBotons = () => setTimeout(() => document.querySelectorAll("[puntuacio]").forEach(boto => boto.style.display = "block"), 300);
+const amagarBotons = () => setTimeout(() => document.querySelectorAll("[puntuacio]").forEach(boto => boto.style.display = "none"), 400);
 
 const rebreAcudit = async () => {  
   try {
@@ -12,15 +11,28 @@ const rebreAcudit = async () => {
         Accept: "application/json",
       },
     });
-    const json = await resposta.json();
-    acudit = json.joke;
-    console.log("Acudit: ", acudit);
-    document.querySelector("#text-acudit").innerHTML = `" ${acudit} "`;
+    const dades = await resposta.json();
+    acudit = dades.joke;
+    console.log("Acudit: ", acudit);    
   } catch (err) {
     console.log(err.message);
   }
+  document.querySelector("#text-acudit").innerHTML = `" ${acudit} "`;
   mostrarBotons();
 }
+
+const rebreInfoMeteo = async () => {
+  let infoMeteo;
+  try {
+    const resposta = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=41.3888&lon=2.159&appid=cf6b3b08d8458fe12e0dddab0b644c2a&lang=CA");
+    const dades = await resposta.json();
+    infoMeteo = dades.weather[0].description;
+  } catch (err) {
+    console.log(err.message);
+  }
+  document.querySelector("#text-meteo").innerHTML = infoMeteo;
+}
+rebreInfoMeteo();
 
 function puntuacio(score) {
   let report = {
@@ -32,6 +44,3 @@ function puntuacio(score) {
   console.table(reportAcudits);
   amagarBotons();
 }
-
-
-
